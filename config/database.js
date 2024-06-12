508907e5a1f2d70b4448ed891dc190ba7cde5e57
Abdubek <zholay.abdubek@gmail.com>
2024-06-12 18:02:52 +0500

    change env variables

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('INS_DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
@@ -49,26 +49,26 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
+        connectionString: env('INS_DATABASE_URL'),
+        host: env('INS_DATABASE_HOST', 'localhost'),
+        port: env.int('INS_DATABASE_PORT', 5432),
+        database: env('INS_DATABASE_NAME', 'strapi'),
+        user: env('INS_DATABASE_USERNAME', 'strapi'),
+        password: env('INS_DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('INS_DATABASE_SSL', false) && {
+          key: env('INS_DATABASE_SSL_KEY', undefined),
+          cert: env('INS_DATABASE_SSL_CERT', undefined),
+          ca: env('INS_DATABASE_SSL_CA', undefined),
+          capath: env('INS_DATABASE_SSL_CAPATH', undefined),
+          cipher: env('INS_DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            'INS_DATABASE_SSL_REJECT_UNAUTHORIZED',
             true
           ),
         },
-        schema: env('DATABASE_SCHEMA', 'public'),
+        schema: env('INS_DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: { min: env.int('INS_DATABASE_POOL_MIN', 2), max: env.int('INS_DATABASE_POOL_MAX', 10) },
     },
     sqlite: {
       connection: {
